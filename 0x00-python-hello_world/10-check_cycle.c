9b24af226b7fe95c4c8dc5b78796eb3e2c5c4f65
alzheimeer <fogniebla@hotmail.com>
2019-09-09 19:33:47 -0500

    feature 10 search loop

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,3 +1,10 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if list has a cycle
+ * @list: pointer to the list
+ * Return: 0 if is no cycle,1 if is a cycle
+ */
 int check_cycle(listint_t *list)
 {
 	listint_t *lento = list, *rap = list;
