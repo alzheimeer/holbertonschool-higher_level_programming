c4749bb17f47774ef587a621ddf22cbd249fc3f6
alzheimeer <fogniebla@hotmail.com>
2019-09-10 16:05:54 -0500

    feature task 12 fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 100):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end='')
+        elif i % 5 == 0:
+            print("Buzz", end='')
+        elif i % 3 == 0:
+            print("Fizz", end='')
+        else:
+            print(i, end='')
+        if i < 100:
+            print(" ", end='')
+        else:
+            print()
