64653d948644b4c40b218fc32b722fd7bb247191
alzheimeer <fogniebla@hotmail.com>
2019-09-09 19:22:11 -0500

    feature 10 search loop

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,36 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
-
-/**
- * check_cycle - find the loop
- * @list: first element a list
- * Return: 0 if no cycle, 1 if there is a cycle
- */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list, *fast = list;
+	listint_t *lento = list, *rap = list;
 
 	if (list == NULL)
 		return (0);
-	while (slow && fast && fast->next)
+	while (lento && rap && rap->next)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
+		lento = lento->next;
+		rap = rap->next->next;
 
-		if (slow == fast)
-	{
-		while (1)
+		if (lento == rap)
 		{
-			slow = fast;
-			while (fast->next != slow && fast->next != list)
-				fast = fast->next;
-			if (fast->next == list)
-				break;
-			list = list->next;
+			while (1)
+			{
+				lento = rap;
+				while (rap->next != lento && rap->next != list)
+					rap = rap->next;
+				if (rap->next == list)
+					break;
+				list = list->next;
+			}
+			return (1);
 		}
-		return (1);
-	}
 	}
 	return (0);
 }
