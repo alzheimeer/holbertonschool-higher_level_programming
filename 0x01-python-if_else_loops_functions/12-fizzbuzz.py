9365c82ed74ce189d7e25f61993368419e7693b7
alzheimeer <fogniebla@hotmail.com>
2019-09-10 16:13:15 -0500

    feature task 12 fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,14 +2,10 @@
 def fizzbuzz():
     for i in range(1, 100):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end='')
+            print("FizzBuzz ", end='')
         elif i % 5 == 0:
-            print("Buzz", end='')
+            print("Buzz ", end='')
         elif i % 3 == 0:
-            print("Fizz", end='')
+            print("Fizz ", end='')
         else:
-            print(i, end='')
-        if i < 100:
-            print(" ", end='')
-        else:
-            print()
+            print("{} ".format(i), end='')
