f3b7cf15b08a7c8850b3a5002eccf1200e5c5d2f
alzheimeer <fogniebla@hotmail.com>
2019-09-09 19:09:25 -0500

    feature 10 search loop

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,22 +9,19 @@
  */
 int check_cycle(listint_t *list)
 {
-  listint_t *slow = list, *fast = list;
+	listint_t *slow = list, *fast = list;
 
-  if (list == NULL)
-    return (0);
-
-  while (slow && fast && fast->next)
-    {
-      slow = slow->next;
-      fast = fast->next->next;
-
-      if (slow == fast)
+	if (list == NULL)
+		return (0);
+	while (slow && fast && fast->next)
 	{
-	  return (1);
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+	{
+		return (1);
 	}
-
-    }
-  return (0);
-
+	}
+	return (0);
 }
