1ef4ef87ce518a1adcd0aef566826aa379bc9a13
alzheimeer <fogniebla@hotmail.com>
2019-09-12 17:00:37 -0500

    feature task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         print("{} arguments:".format(lenargv - 1))
         for i in range(1, lenargv):
             print("{}: {}".format(i, sys.argv[i]))
-            lenargv += 1
+
