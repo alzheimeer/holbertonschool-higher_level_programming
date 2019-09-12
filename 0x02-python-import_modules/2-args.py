b6dd8e08b972cc5e30acd3e3dd2adc25766fb44c
alzheimeer <fogniebla@hotmail.com>
2019-09-12 17:09:08 -0500

    feature task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    lenargv = len(sys.argv)
-    if lenargv == 1:
-        print("{} arguments.".format(lenargv - 1))
-    elif lenargv == 2:
-        print("{} argument:".format(lenargv - 1))
-        print("{}: {}".format(lenargv - 1, sys.argv[1]))
+    lenargs = len(sys.argv)
+    if lenargs == 1:
+        print("{} arguments.".format(lenargs - 1))
+    elif lenargs == 2:
+        print("{} argument:".format(lenargs - 1))
+        print("{}: {}".format(1, sys.argv[1]))
     else:
-        print("{} arguments:".format(lenargv - 1))
-        for i in range(1, lenargv):
+        print("{} arguments:".format(lenargs - 1))
+        for i in range(1, lenargs):
             print("{}: {}".format(i, sys.argv[i]))
-
