72df07fd64e96250f25608acc8d2102b2d12fee1
alzheimeer <fogniebla@hotmail.com>
2019-09-12 12:07:23 -0500

    feature 10 search loop

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-add = __import__('0-add').add
-
-a = 1
-b = 2
-print("{}+{}={}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
