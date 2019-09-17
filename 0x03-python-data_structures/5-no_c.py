7afa448e891550189cf17defa2ea9672a9b44e59
alzheimeer <fogniebla@hotmail.com>
2019-09-17 14:21:11 -0700

    fix task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-        my_string = (my_string.replace('cC', ''))
-        return my_string
+        x = (my_string.replace('cC', ''))
+        return x
