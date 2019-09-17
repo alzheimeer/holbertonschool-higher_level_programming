0b6d06cc6233d43bbebaddc67168ad49e156694d
alzheimeer <fogniebla@hotmail.com>
2019-09-17 15:20:11 -0700

    fix task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for elem in row:
-            print("{:d}".format(int(elem)), end=' ')
-        print("")
+        for item in row:
+            print('\n'.join([' '.join(['{:d}'.format(item)])]), end=' ')
+        print()
