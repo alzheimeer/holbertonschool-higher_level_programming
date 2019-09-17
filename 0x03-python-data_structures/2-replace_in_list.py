d801631a3837727334a1d18aa4a9b7f60f8fea73
alzheimeer <fogniebla@hotmail.com>
2019-09-17 13:06:28 -0700

    feature task 2 replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,8 +2,8 @@
 def replace_in_list(my_list, idx, element):
     lenx = len(my_list)
     if idx < 0:
-        return
+        return (my_list)
     elif idx > lenx - 1:
-        return
+        return (my_list)
     my_list[idx] = element
     return (my_list)
