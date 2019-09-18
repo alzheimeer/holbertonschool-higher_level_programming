49f8be1d4593829c80878d104833911533fdff45
alzheimeer <fogniebla@hotmail.com>
2019-09-17 17:59:55 -0700

    feature task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = my_list.copy()
-
-    for i in range(len(new_list)):
-        if new_list[i] % 2 == 0:
-            new_list[i] = True
+    nl = my_list.copy()
+    for i in range(len(nl)):
+        if nl[i] % 2 != 0:
+            nl[i] = False
         else:
-            new_list[i] = False
-    return(new_list)
+            nl[i] = True
+    return(nl)
