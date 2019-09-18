0922cb5245170ad8aead2af67ab4a9330b5ca241
alzheimeer <fogniebla@hotmail.com>
2019-09-17 22:37:55 -0700

    up advanced no complete

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - Function that prints basic info about python objects
+ * @p: PyObject
+ */
+
+void print_python_list_info(PyObject *p)
+{
+
+
+
+}
