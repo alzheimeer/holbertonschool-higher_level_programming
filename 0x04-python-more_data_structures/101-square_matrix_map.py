962d1cc4618a12e88092d3548ad9f09e0403d1f8
alzheimeer <fogniebla@hotmail.com>
2019-09-19 16:58:00 -0700

    fix task 101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda x: list(map(jf, x)), matrix))
-
-
-def jf(x):
-    x = x ** 2
-    return(x)
+    return list(map(lambda a: list(map(lambda b: b*b, a)), matrix))
