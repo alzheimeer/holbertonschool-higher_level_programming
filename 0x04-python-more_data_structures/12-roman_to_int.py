a8f0fe267344f6c2a238b34925d56df7f5adba59
alzheimeer <fogniebla@hotmail.com>
2019-09-19 21:16:23 -0700

    fix task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(num):
+    if num is None or type(num) is not str:
+        return 0
     listax = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     r = 0
     for i, c in enumerate(num):
