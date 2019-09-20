bf140b7043c6bdc81b5ee85bd203fc88359ecb3a
alzheimeer <fogniebla@hotmail.com>
2019-09-19 21:01:52 -0700

    feature task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def roman_to_int(num):
+    listax = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    r = 0
+    for i, c in enumerate(num):
+        if (i+1) == len(num) or listax[c] >= listax[num[i+1]]:
+            r += listax[c]
+        else:
+            r -= listax[c]
+    return r
