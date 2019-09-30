d63c6b178562530f7c33d8a6cd16bc5a6f1d04dc
alzheimeer <fogniebla@hotmail.com>
2019-09-30 13:54:51 -0500

    fix task 0 execption

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    s = 0
     for i in range(x):
         try:
             print(my_list[i], end='')
-            i += 1
-        except IndexError:
+            s += 1
+        except:
             break
     print()
-    return i
+    return s
