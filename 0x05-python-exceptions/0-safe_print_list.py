8faf6fcfdd2940f7318168dcc3b2f996f316a876
alzheimeer <fogniebla@hotmail.com>
2019-09-30 13:46:22 -0500

    feature task 0 execption

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for i in range(x):
+        try:
+            print(my_list[i], end='')
+            i += 1
+        except IndexError:
+            break
+    print()
+    return i
