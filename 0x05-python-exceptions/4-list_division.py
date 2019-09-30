ed0edc1997d1de21f08c9361f290d3cdbb8f6f68
alzheimeer <fogniebla@hotmail.com>
2019-09-30 15:40:21 -0700

    fix task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,4 +16,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             new_list.append(0)
             continue
-    return new_list
+        finally:
+            return new_list
