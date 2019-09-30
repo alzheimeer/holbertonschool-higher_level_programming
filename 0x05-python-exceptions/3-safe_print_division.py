075320286b60690a2a4958e4fe4819fc71e3852e
alzheimeer <fogniebla@hotmail.com>
2019-09-30 14:38:45 -0500

    feature task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        d = a / b
+    except:
+        d = None
+    finally:
+        print("Inside result: {}".format(d))
+        return d
