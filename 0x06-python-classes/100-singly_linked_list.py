c1a109c92b1f08eafe624ee42c8a6c85679b98bb
alzheimeer <fogniebla@hotmail.com>
2019-10-02 22:41:08 -0500

    fix 100 next for next_node

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class Node:
-    def __init__(self, data, next=None):
+    def __init__(self, data, next_node=None):
         self.data = data
-        self.next = next
+        self.next_node = next_node
 
     @property
     def data(self):
@@ -15,16 +15,14 @@ class Node:
         self.__data = value
 
     @property
-    def next(self):
-        return self.__next
+    def next_node(self):
+        return self.__next_node
 
-    @next.setter
-    def next(self, value):
-        if value is None or isinstance(value, Node):
-            self.__next = value
-        else:
-            raise TypeError("next must be a Node object")
-
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node_node must be a Node object")
+        self.__next_node = value
 
 class SinglyLinkedList:
     def __init__(self):
@@ -36,19 +34,19 @@ class SinglyLinkedList:
         temp = Node(value)
 
         if actual is None:
-            temp.__next = None
+            temp.__next_node = None
             self.__head = temp
 
         else:
             while actual is not None and value > actual.data:
                 previo = actual
-                actual = actual.__next
+                actual = actual.__next_node
             if previo is None:
-                temp.__next = self.__head
+                temp.__next_node = self.__head
                 self.__head = temp
             else:
-                previo.__next = temp
-                temp.__next = actual
+                previo.__next_node = temp
+                temp.__next_node = actual
 
     def __str__(self):
 
@@ -56,5 +54,5 @@ class SinglyLinkedList:
         temp = self.__head
         while temp is not None:
             n.append(str(temp.data))
-            temp = temp.__next
+            temp = temp.__next_node
         return ('\n'.join(n))
