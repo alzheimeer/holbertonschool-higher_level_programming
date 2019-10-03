ab94260b8ed999e1a575b886878825563b9cc610
alzheimeer <fogniebla@hotmail.com>
2019-10-03 12:43:49 -0700

    test mains

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer")
+
+    return a+b
--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
 print(add_integer(1, 2))
