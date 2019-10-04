003f6cd7c45d32a4b807f0b9b8439cf7e34547cf
alzheimeer <fogniebla@hotmail.com>
2019-10-04 16:01:56 -0700

    feature task 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    """
+    Args:
+        first_name (str): the first name
+        last_name (str, optional): the last name
+    Raises:
+        TypeError: if the first_name or last_name are not strings
+    """
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
