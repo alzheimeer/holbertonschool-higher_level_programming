fb758e4402b96a5abf89e14472ec426f83894650
alzheimeer <fogniebla@hotmail.com>
2019-10-04 16:13:54 -0700

    task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Documentation for a square printing function"""
+def print_square(size):
+    """print a square
+    Args:
+        size (int): the side length of the square
+    Raises:
+        TypeError: when the size passed in is not an integer
+        ValueError: when the size is less than 0 (ie negative)
+    """
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    if not isinstance(size, float) and size < 0:
+        raise TypeError('size must be an integer')
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print('')
