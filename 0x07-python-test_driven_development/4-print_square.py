2dc0de976417244aaf145e0ca8392f4f844f05cb
alzheimeer <fogniebla@hotmail.com>
2019-10-05 09:09:20 -0700

    fix task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,10 +14,11 @@ def print_square(size):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
+
     if not isinstance(size, float) and size < 0:
         raise TypeError('size must be an integer')
 
-    for i in range(size):
-        for j in range(size):
+    for c1 in range(size):
+        for c2 in range(size):
             print("#", end='')
         print('')
