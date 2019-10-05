5225ff7bc143ee596664b7dcd07f9365c090519f
alzheimeer <fogniebla@hotmail.com>
2019-10-04 19:31:00 -0700

    feature txt task 5 and fix py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,12 +6,17 @@ def text_indentation(text):
     """
     if not isinstance(text, str) or text is None or len(text) < 0:
         raise TypeError('text must be a string')
-
     datos = text.replace('?', '?pepe')
     datos1 = datos.replace('.', '.pepe')
     datos2 = datos1.replace(':', ':pepe')
     t = datos2.split('pepe')
+    len1 = len(t)
+    a = 0
+    if (t[len1-1]) == "":
+        len1 -= 1
     for i in t:
+        a += 1
         i1 = i.strip()
         print (i1)
-        print()
+        if a < len1:
+            print()
