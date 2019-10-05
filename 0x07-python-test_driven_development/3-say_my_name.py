cb99c3871607577385b68eaef99614f87c79c424
alzheimeer <fogniebla@hotmail.com>
2019-10-05 08:44:44 -0700

    fix say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,9 +7,9 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: if the first_name or last_name are not strings
     """
-    if type(first_name) is not str:
+    if first_name is None or type(first_name) is not str:
         raise TypeError('first_name must be a string')
-    if type(last_name) is not str:
+    if last_name is None or type(last_name) is not str:
         raise TypeError('last_name must be a string')
 
     print("My name is {:s} {:s}".format(first_name, last_name))
