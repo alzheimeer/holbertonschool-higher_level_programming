2e48a1c03d7951ca391d940c5750d1d32776bddc
alzheimeer <fogniebla@hotmail.com>
2019-10-06 11:39:31 -0700

    fix 101

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-module that has matrix operations
-"""
+
+import numpy
 
 
 def lazy_matrix_mul(m_a, m_b):
     """ Dot product with numpy"""
-    import numpy
     return numpy.matmul(m_a, m_b)
