f755f48c03c37295da8fbe5a65ee123493f185cd
alzheimeer <fogniebla@hotmail.com>
2019-10-15 18:56:22 -0500

    feature task 100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class MyInt(int):
+    '''inverted'''
+    def __init__(self, size):
+        self.__size = size
+
+    def __eq__(self, other):
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        return int.__eq__(self, other)
