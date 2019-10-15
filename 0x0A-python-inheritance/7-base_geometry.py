5f5cf9787e051243f0476e341778ed359dbe6c0b
alzheimeer <fogniebla@hotmail.com>
2019-10-15 16:47:10 -0500

    feature task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry():
+    """Class"""
+    def area(self):
+        """ Raise exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''validates value'''
+        if type(value) is not int:
+            raise TypeError(name+ "must be an integer")
+        if value <= 0:
+            raise ValueError(name+ "must be greater than 0")
