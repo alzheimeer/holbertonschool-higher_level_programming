bee7cb90b05c5915bb56d8026dcbf3e8abcdc68e
alzheimeer <fogniebla@hotmail.com>
2019-10-16 17:55:02 -0500

    fix task 10 vars

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import json
 
 
 def class_to_json(obj):
-    '''returns the dictionary description with simple data structure'''
-
+    """returns the dictionary description with simple data structure
+    """
     return vars(obj)
