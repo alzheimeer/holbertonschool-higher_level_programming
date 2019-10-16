13fb262cda811e9a0f591c77552d1ffffb6e0c29
alzheimeer <fogniebla@hotmail.com>
2019-10-16 14:56:55 -0500

    feature task 8 json

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+
+
+def load_from_json_file(filename):
+    '''creates an Object from a “JSON file”'''
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
