56a1ffaa7216059ef9f591c2465aa5ce8d0cb345
alzheimeer <fogniebla@hotmail.com>
2019-10-16 10:31:48 -0500

    feature 7 task json write

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''function that writes an Object to a file, using a JSON representation'''
+    with open(filename, 'w', encoding="utf-8") as f:
+                json.dump(my_obj, f)
