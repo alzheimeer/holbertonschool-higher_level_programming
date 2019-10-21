d2d68e04fd7e33db87e5c1fe4ae90f67722c80a4
alzheimeer <fogniebla@hotmail.com>
2019-10-20 20:56:57 -0700

    feature task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,7 +74,11 @@ class Rectangle(Base):
 
     def display(self):
         '''prints in stdout'''
+        for m in range(self.__y):
+            print()
         for i in range(self.__height):
+            for f in range(self.__x):
+                print(' ', end='')
             for j in range(self.__width):
                 print('#', end='')
             print()
