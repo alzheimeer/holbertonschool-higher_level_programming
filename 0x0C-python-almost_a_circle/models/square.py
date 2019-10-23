32577e95733632ee3e83e3521cd83993fa83b5fd
alzheimeer <fogniebla@hotmail.com>
2019-10-22 20:50:09 -0500

    fix 12 task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -31,24 +31,16 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """Updating variable arguments"""
-        if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.size = args[1]
-            if len(args) >= 4:
-                self.x = args[2]
-            if len(args) == 5:
-                self.y = args[3]
+        a = ['id', 'size', 'x', 'y']
+        count = 0
+        if not args:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+                count += 1
         else:
-            if "id" in kwargs:
-                self.id = kwargs["id"]
-            if "size" in kwargs:
-                self.size = kwargs["size"]
-            if "x" in kwargs:
-                self.x = kwargs["x"]
-            if "y" in kwargs:
-                self.y = kwargs["y"]
+            for c in args:
+                setattr(self, a[count], c)
+                count += 1
 
     def to_dictionary(self):
         """dictionary of a Square"""
