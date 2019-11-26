cb2cb68a8815aba7d81ccabe7d6886cbd9078e56
alzheimeer <fogniebla@hotmail.com>
2019-11-26 17:01:26 -0500

    fix task basic refresh

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -6,3 +6,4 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'hbtn_0d_2';
 GRANT SELECT ON hbtn_0d_2. * TO user_0d_2@localhost;
+FLUSH PRIVILEGES;
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -3,4 +3,5 @@
 -- and must be unique name VARCHAR(256)
 -- The database name will be passed as an argument of the mysql command
 -- If the table unique_id already exists, your script should not fail
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE,VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id
+(id INT DEFAULT 1 UNIQUE,name VARCHAR(256));
