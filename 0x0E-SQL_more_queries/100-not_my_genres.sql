706b2b0f08192208ff0cc764a1254f6b852b2b7a
alzheimeer <fogniebla@hotmail.com>
2019-11-26 16:50:22 -0500

    features adv

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- uses hbtn_0d_tvshows db to list all genres not linked to the show Dexter
+-- The tv_shows table contains only one record where
+-- title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN
+(SELECT tv_genres.name FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter')
+ORDER BY tv_genres.name ASC;
