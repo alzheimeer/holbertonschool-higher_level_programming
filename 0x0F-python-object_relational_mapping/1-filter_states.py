8a8123a17f5c47cbfbc30b3b08bc42c5db369665
alzheimeer <fogniebla@hotmail.com>
2019-12-13 15:43:54 -0500

    feature task 1 filter N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""  lists all states with a name starting with N (upper N)
+     from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                                user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = conn.cursor()
+    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for data in result:
+        print(data)
+    cursor.close()
+    conn.close()
