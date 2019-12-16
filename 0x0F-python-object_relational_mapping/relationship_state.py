6137079af499c99d038d11c59e28ebf12f4ea411
alzheimeer <fogniebla@hotmail.com>
2019-12-16 15:30:25 -0500

    feature adv task 15

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""State ORM object"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ state class """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement="auto")
+
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", cascade='delete', backref="state")
