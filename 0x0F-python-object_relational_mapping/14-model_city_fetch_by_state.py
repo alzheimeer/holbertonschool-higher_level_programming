b056bef09151319c8cc0f536c03d351a83de4d0a
alzheimeer <fogniebla@hotmail.com>
2019-12-15 21:08:31 -0500

    fix task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,12 +4,11 @@
 import sys
 from model_state import Base, State
 from model_city import City
-
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
@@ -18,7 +17,8 @@ if __name__ == "__main__":
     session = Session()
 
     for city, state in session.query(City, State).\
-             filter(City.state_id == State.id).order_by(City.id):
-        print("{}: {} {}".format(state.name, city.id, city.name))
+            filter(City.state_id == State.id).\
+            order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """City ORM object"""
 
+from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
 
 
 class City(Base):
     """ city class """
     __tablename__ = 'cities'
-    id = Column(Integer, nullable=False, autoincrement=True,
-                unique=True, primary_key=True)
-    name =  Column(String(128), nullable=False)
-    state_id =  Column(Integer, ForeignKey("states.id"), nullable=False)
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement="auto")
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
