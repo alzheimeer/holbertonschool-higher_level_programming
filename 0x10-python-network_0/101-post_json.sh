f8eee62d35670b5dcdc7848f5973aa02e7949b2d
alzheimeer <fogniebla@hotmail.com>
2020-01-07 14:36:22 -0500

    fix 101 task

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument,
 # and displays the body of the response.
-curl -s -H "Content-Type: application/json" --request POST --data @"$2" "$1"
+curl -sH "Content-Type: application/json" --request POST --data @"$2" "$1"
