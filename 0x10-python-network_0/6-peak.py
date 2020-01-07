987812adb71b3477835548b485f9f90c04992c74
alzheimeer <fogniebla@hotmail.com>
2020-01-07 12:35:52 -0500

    fix 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,10 +4,6 @@ def find_peak(list_of_integers):
         return None
     else:
         v = len(list_of_integers)
-        mid = v//2
         list_of_integers.sort()
-        max_value = list_of_integers[mid]
-        for item in list_of_integers[mid:v]:
-            if item > max_value:
-                max_value = item
+        max_value = list_of_integers[v-1]
         return (max_value)
