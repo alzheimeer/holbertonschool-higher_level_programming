39a65794c43afbdf774ce835d86c136ab3c708e9
alzheimeer <fogniebla@hotmail.com>
2020-01-13 16:51:57 -0500

    feature task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" requests to get header info"""
+import requests
+from sys
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    print(response.headers.get('X-Request-Id'))
