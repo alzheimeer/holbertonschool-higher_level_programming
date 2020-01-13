8b5cf1672f88fe6077eb7c1387d61c1875a33194
alzheimeer <fogniebla@hotmail.com>
2020-01-13 15:27:27 -0500

    feature task 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-""" Fetches https://intranet.hbtn.io/status """
+""" https://intranet.hbtn.io/status """
 
-import urllib.request
+from urllib import request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
+with request.urlopen("https://intranet.hbtn.io/status") as response:
+    r = response.read()
     print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    print("\t- type: {}".format(type(r)))
+    print("\t- content: {}".format(r))
+    print("\t- utf8 content: {}".format(r.decode("utf-8")))
---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""sends a request and displays value X-Request-Id in header response. 
+   response.info() show all information and with an if find X-request or
+   print(response.getheader("X-Request-Id"))
+"""
+
+from urllib import request
+import sys
+
+url = sys.argv[1]
+with request.urlopen(url) as response:
+    print(response.getheader("X-Request-Id"))
