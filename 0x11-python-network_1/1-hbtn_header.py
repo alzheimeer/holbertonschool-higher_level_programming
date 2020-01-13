865b3bb1830af9dd3a09bf87090967c5f2784146
alzheimeer <fogniebla@hotmail.com>
2020-01-13 15:35:02 -0500

    fix tash 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""sends a request and displays value X-Request-Id in header response. 
+"""sends a request and displays value X-Request-Id in header response.
    response.info() show all information and with an if find X-request or
    print(response.getheader("X-Request-Id"))
 """
@@ -9,4 +9,6 @@ import sys
 
 url = sys.argv[1]
 with request.urlopen(url) as response:
-    print(response.getheader("X-Request-Id"))
+    header = response.info()
+if "X-Request-Id" in header:
+    print(header['X-Request-Id'])
