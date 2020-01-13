00fde209b27f88b9cbb384b9cbab7d14a7b9df0e
alzheimeer <fogniebla@hotmail.com>
2020-01-13 17:11:12 -0500

    fix task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    if response.status_code > 400:
+    if r.status_code > 400:
         print("Error code:", r.status_code)
     else:
         print(r.text)
