7af852d6be4dd7d702533fc32f20f3bdbc5fb4aa
alzheimeer <fogniebla@hotmail.com>
2020-01-13 17:02:01 -0500

    fix task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
-Takes in a URL and an email, sends a POST request to the passed URL
-with the email as a parameter, and displays the body of the response
-(decoded in utf-8)
+takes in a URL and an email address, sends a POST request to the passed URL
+ with the email as a parameter, and finally displays the body of the response.
 """
 import requests
 import sys
 
 if __name__ == "__main__":
-    response = requests.post(sys.argv[1], data={'email': argv[2]})
-    print(response.text)
+    r = requests.post(sys.argv[1], {'email': argv[2]})
+    print(r.text)
