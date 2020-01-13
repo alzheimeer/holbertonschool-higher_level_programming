b046b0f769b9e0a468cd1612c4e41495fe8a7c22
alzheimeer <fogniebla@hotmail.com>
2020-01-13 16:35:52 -0500

    fix task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,6 +9,6 @@ import sys
 
 if __name__ == "__main__":
     a = sys.argv[1]
-    b = parse.urlencode({"email": sys.argv[2]}).encode()
+    b = parse.urlencode({"email": sys.argv[2]}).encode('utf8')
     with request.urlopen(a, b) as response:
         print(response.read().decode('utf-8'))
