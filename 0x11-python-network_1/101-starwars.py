bc71c845467eea1f7a0ff0b46d41806eee88fae7
alzheimeer <fogniebla@hotmail.com>
2020-01-13 23:06:59 -0500

    fix 101 task

--- 0x11-python-network_1/101-starwars.py
+++ 0x11-python-network_1/101-starwars.py
@@ -7,10 +7,17 @@ import sys
 
 if __name__ == "__main__":
     url = 'https://swapi.co/api/people'
-    params = (('search', sys.argv[1]),)
+    params = {'search': sys.argv[1]}
     r = requests.get(url, params=params)
     d = r.json()
     print("Number of results: {}".format(d['count']))
     c = d['results']
     for x in c:
         print(x['name'])
+
+    # Loop for reading next pages
+    while d['next'] is not None:
+        d = requests.get(d['next']).json()
+        h = d['results']
+        for c in h:
+            print(c['name'])
