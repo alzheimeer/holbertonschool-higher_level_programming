318480d8c62fd848ed3b6f24c372c42789b9e574
alzheimeer <fogniebla@hotmail.com>
2020-02-04 17:12:42 -0500

    feature task 9 first function

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+//  prints the addition of 2 integers
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
+}
+
+add(a, b);
