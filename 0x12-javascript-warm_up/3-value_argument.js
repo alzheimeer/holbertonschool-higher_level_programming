cc18fe7ffb67a765a3f08c0afec4288452a3dcca
alzheimeer <fogniebla@hotmail.com>
2020-02-04 15:14:38 -0500

    feature task 3 print value argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 //prints the first argument passed to it:
 
-if (process.argv.length < 3) {
+if (typeof process.argv[2] === 'undefined') {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else {
   console.log(process.argv[2]);
 }
