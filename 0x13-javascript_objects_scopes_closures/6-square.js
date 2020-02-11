a549410f583531f8edba97bde3061200b2e34801
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-02-10 23:17:43 -0500

    feature task 5 and 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const Square2 = require('./5-square.js');
+
+module.exports = class Square extends Square2 {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    let i = 0;
+    for (; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
