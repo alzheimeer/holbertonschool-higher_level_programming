1b4b5832f57dad4a0439c38c43d1d9d5240178bf
alzheimeer <fogniebla@hotmail.com>
2020-02-11 13:49:29 -0500

    feature task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const request = require('request');
+const url = `https://swapi.co/api/films/${process.argv[2]}/`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
