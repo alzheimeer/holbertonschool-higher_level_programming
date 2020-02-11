81945c83590fad4b76ecfcfb38f88e1a1416e1e1
alzheimeer <fogniebla@hotmail.com>
2020-02-11 13:27:49 -0500

    feature task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+var https = require('https');
+const request = https.get(process.argv[2], function (response) {
+  console.log(response.statusCode);
+});
+
+request.on('error', function (error) {
+  console.error(error.status);
+});
