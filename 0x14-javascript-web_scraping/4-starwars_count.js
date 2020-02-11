0172d3953acf790f6b95b5143e0c3f88744197d6
alzheimeer <fogniebla@hotmail.com>
2020-02-11 14:37:17 -0500

    data

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const request = require('request');
+/** npm install request --save */
+const url = `https://swapi.co/api/films/${process.argv[2]}/`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
