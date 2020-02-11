0530e7c7fb82375961fbdec97618503c5451012c
alzheimeer <fogniebla@hotmail.com>
2020-02-11 16:41:15 -0500

    feature task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-/** npm install request --save */
-const url = `https://swapi.co/api/films/${process.argv[2]}/`;
+const url = process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    console.log(JSON.parse(body).title);
+    const films = JSON.parse(body).results;
+    let n = 0;
+    for (const X of films) {
+      for (const char of X) {
+        if (char.search('/18/') > 0) {
+          n++;
+        }
+      }
+    }
+    console.log(n);
   }
 });
