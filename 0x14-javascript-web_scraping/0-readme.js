1fca17c2a457c610acf84a1b15adaddeb3b62ea3
alzheimeer <fogniebla@hotmail.com>
2020-02-11 11:45:03 -0500

    feature task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+/* File System Object */
+const fs = require('fs');
+/* Read File */
+try {
+  const data = fs.readFileSync(process.argv[2], 'utf8');
+  process.stdout.write(data);
+} catch (e) {
+  console.error(e);
+}
