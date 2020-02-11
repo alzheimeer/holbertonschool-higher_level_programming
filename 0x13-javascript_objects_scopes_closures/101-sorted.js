2e9b0baf216d251ca86bcff6abf82d5263295b69
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-02-11 00:01:24 -0500

    fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// concatenates the 2 files passed as arguments
 const fs = require('fs');
 const A = fs.readFileSync(process.argv[2], 'utf-8');
 const B = fs.readFileSync(process.argv[3], 'utf-8');
