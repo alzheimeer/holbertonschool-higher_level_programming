359633a621469868b5af9785c572872d5cff3029
alzheimeer <fogniebla@hotmail.com>
2020-02-11 17:06:58 -0500

    feature task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const fs = require('fs');
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const body1 = body;
+    fs.writeFile(process.argv[3], body1, 'utf-8', (error) => {
+      error && console.log(error);
+    });
+  }
+});
