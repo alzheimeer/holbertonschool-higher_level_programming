5d8b8df3a791057e7de50639d868e690760218d7
E. Carlos Mauricio Quintero <fogniebla@hotmail.com>
2020-02-10 19:23:49 -0500

    feature task 4 double and rotate

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    while (this.height) {
+      console.log('X'.repeat(this.width));
+      this.height--;
+    }
+  }
+
+  rotate () {
+    const swap = this.width;
+    this.width = this.height;
+    this.height = swap;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+};
