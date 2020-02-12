3b81a31099407456c75ae418ee79bffdc4a97d27
alzheimeer <fogniebla@hotmail.com>
2020-02-12 00:06:21 -0500

    task all

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+const url = `https://swapi.co/api/films/${process.argv[2]}/`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const actor of characters) {
+      request(actor, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const task of tasks) {
+      if (task.completed === true) {
+        if (!(task.userId in completed)) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    //    console.log(JSON.stringify(completed));
+    console.log(completed);
+  }
+});
