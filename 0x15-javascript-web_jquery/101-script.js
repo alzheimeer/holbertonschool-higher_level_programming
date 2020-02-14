874e02e2a35f870d249bcc3afa6a7c579dd3b8a1
alzheimeer <fogniebla@hotmail.com>
2020-02-13 21:27:30 -0500

    feature 101

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,5 @@
+$(() => {
+  $('DIV#add_item').click(() => $('UL.my_list').append('<li>Item</li>'));
+  $('DIV#remove_item').click(()=>$('UL.my_list').children().last().remove());
+  $('DIV#clear_list').click(() => $('UL.my_list').empty());
+});
